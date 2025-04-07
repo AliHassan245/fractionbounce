3dd660190dc04a9ac569447c31435f1b3109d80d
Ali Hassan <141296395+AliHassan245@users.noreply.github.com>
2025-04-07 22:21:15 +0500

    Update bar.py

--- bar.py
+++ bar.py
@@ -75,11 +75,7 @@ class Bar():
                            self._height - BAR_HEIGHT * self._scale,
                            svg_str_to_pixbuf(mark))
         self.mark.set_layer(1)
-        f = 200
-        target_x = int(f - self.mark.mark_width() / 2)  # f being the target x-position calculated elsewhere
-        target_y = int(self.bar.bar_y() + self._mark_offset(f))
-        self.mark.move((target_x, target_y))
-      
+     
     def update_indicator(self, correct):
 
         color = '#00FF00' if correct else '#FF0000'
